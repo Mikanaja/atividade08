7f337aab095349b75662f327e29c01c33236a60d
Mikhael Machado Fernandes Maia <65919190+Mikanaja@users.noreply.github.com>
2024-04-26 18:06:30 -0300

    Create ContaBancaria.java

---
+++ ContaBancaria.java
@@ -0,0 +1,28 @@
+class SaldoInsuficienteException extends Exception {
+    public SaldoInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
+
+public class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) throws SaldoInsuficienteException {
+        if (valor > saldo) {
+            throw new SaldoInsuficienteException("Saldo insuficiente para sacar o valor desejado.");
+        }
+        saldo -= valor;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
